82c15e13c41e96ecbe963d020c2ee0b12e4e98d7
“Spencer <srye1140@gmail.com>
2023-02-11 12:38:11 -0800

    Adds functional code for models and routes. Adds pseudo code for controllers.

---
+++ controllers/thoughtController.js
@@ -0,0 +1,20 @@
+// Get all thoughts
+getThoughts
+
+// Get single thought by id
+getSingleThought
+
+// Post new thought (push thoughts id to user's thoughts array field)
+createThought
+
+// Put for update thought by id
+updateThought
+
+// Delete to remove thought by id
+deleteStudent
+
+// Post to create reaction stored in thought's reaction array field
+addReaction
+
+// Delete to pull and remove reaction by reaction's id
+removeReaction
---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+// Get all users
+getUsers
+
+// Get single user by ID
+getSingleUser
+
+// Post new user
+createUser
+
+// Put for update user by id
+updateUser
+
+// Delete to remove user by id
+deleteUser
+
+// BONUS - Remove users thoughts when user is deleted
+
+// Post to add new friend to user's friend list
+addFriend
+
+// Delete to remove friend from user's friend list
+removeFriend
